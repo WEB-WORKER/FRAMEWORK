50928665c477590f28b3294df1b9922b4cdfa744
WEB-WORKER <webworker100@gmail.com>
2018-07-11 08:31:16 +0300

    add slider

--- app/js/main.js
+++ app/js/main.js
@@ -88,9 +88,53 @@ $(document).ready(function(){
 			    	innerLink.addClass('active');
 			    	innerContents.stop(true, true).slideUp(duration).removeClass('active');
 			    	innerContent.stop(true, true).slideDown(duration).addClass('active');
-				}		    
-
+				}  
 		});
+
+		//slider
+		$('.slider__controls-button').on('click', function(e){
+			e.preventDefault();
+
+			var $this = $(this),
+			container = $this.closest('.slider'),
+			list = container.find('.slider__list'),
+			items = container.find('.slider__item'),
+			activeSlide = items.filter('.active'),
+			nextSlide = activeSlide.next(),
+			prevSlide = activeSlide.prev(),
+			firstSlide = items.first(),
+			lastSlide = items.last(),
+			sliderOffset = container.offset().left,
+			reqPos = 0;
+			
+			if ($this.hasClass('slider__controls-button-next')){				
+				if (nextSlide.length){
+					findReqPos(nextSlide);					
+					removeActiveClass(nextSlide);
+				} else {
+					findReqPos(firstSlide);					
+					removeActiveClass(firstSlide);
+				}
+			} else {
+				// "$this.hasClass('slider__controls-button-prev')"
+				if (prevSlide.length){
+					findReqPos(prevSlide);					
+					removeActiveClass(prevSlide);
+				} else {
+					findReqPos(lastSlide);					
+					removeActiveClass(lastSlide);
+				}
+			}			
+			list.css('left', '-=' + reqPos + 'px');
+			
+			function removeActiveClass (reqSlide){
+				reqSlide.addClass('active').siblings().removeClass('active');
+			}
+			function findReqPos (slide){
+				reqPos = slide.offset().left - sliderOffset;
+			}
+		});
+
 	
 });//end jQuery
 
